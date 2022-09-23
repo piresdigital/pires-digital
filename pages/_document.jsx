6a5f690ba1b96770cd0080dbed97933faf8388f3
Dhavidy Pires <dhavidy.pires@outlook.com>
2022-09-24 10:38:24 +1200

    Changed og:title and description

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,17 +8,18 @@ export default function Document() {
       <meta charSet="utf-8" />
       <meta property="og:locale" content="en_NZ" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Pires Digital | Creative Digital Agency | Ad Creative Services" />
-      <meta property="og:description" content="As a creative advertising agency in Christchurch New Zealand, we offer a full range of ad creative services that help your business with all of its advertising and designing needs. Get a free quote today!" />
+      <meta property="og:title" content="Pires Digital | Web Design Agency based in Christchurch NZ" />
+      <meta property="og:description" content="As a creative web agency in Christchurch New Zealand, we offer a range of creative web services that help your business with all of its web designing needs. Get a free quote today!" />
       <meta property="og:url" content="https://piresdigital.com/" />
       <meta property="og:image" content="/img/open-graph.gif" />
       <meta property="og:image:width" content="1080" />
       <meta property="og:image:height" content="1080" />
       <meta name="geo.placename" content="Christchurch" />
       <meta name="geo.region" content="NZ" />
+
       <link rel="icon" type="image/png" href="img/favicon.png"></link>
 
-      <script type='text/javascript' src='/main.js'></script>
+      <base href='http://127.0.0.1:3000/' target='_self' />
       
       </Head>
       <body>
